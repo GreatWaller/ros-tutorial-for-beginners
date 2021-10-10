b214261fe4976407276995d6885fb6c628ff60f1
GreatWaller <zhangxuan4422@live.cn>
2021-10-10 08:46:32 +0800

    Add: simple pub_sub

---
+++ src/ros_topic/script/simple_subscriber.py
@@ -0,0 +1,19 @@
+import rospy
+from rospy.timer import sleep
+from std_msgs.msg import String
+
+class SimpleSubscriber():
+    def __init__(self) -> None:
+        self.node=rospy.init_node('simple_listener')
+        # self.rate = rospy.Rate(1)
+        self.sub= rospy.Subscriber('simple_talker',String,self.callback,queue_size=1)
+
+    def callback(self,msg):
+        print(msg)
+        # self.rate.sleep()
+        sleep(1)
+
+if __name__ == '__main__':
+    SimpleSubscriber()
+    rospy.spin()
+    
