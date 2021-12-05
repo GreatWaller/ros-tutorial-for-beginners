34be4f21c7d115ba6002c1a1e07621db9c287f62
GreatWaller <zhangxuan4422@live.cn>
2021-12-05 23:10:39 +0800

    Add: camera distortion

--- src/transform_tutorial/src/basic.cpp
+++ src/transform_tutorial/src/basic.cpp
@@ -12,5 +12,17 @@ int main(int argc, char const *argv[])
     auto p_o = q.matrix() * p_c + t;
     std::cout << p_o << std::endl;
 
+    Eigen::Matrix3d R;
+    R << 0.9999702331747733, 0.004739171650665555, 0.00608876148778472,
+
+        -0.004808490243971323, 0.9999232173912479, 0.01142093444114481,
+
+        -0.006034168208066053, -0.01144987222639725, 0.9999162411122423;
+    Eigen::Vector3d euler = R.eulerAngles(2, 1, 0);
+    std::cout << euler << std::endl;
+    // 3.13678
+    // 3.13556
+    // 3.13014
+
     return 0;
 }
